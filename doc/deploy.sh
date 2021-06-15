dd06eb3f324260d62b5fc72d95d710802840fddb
Luís Fernando Gaido <luisfernandogaido@gmail.com>
2021-06-14 21:25:05 -0300

    deploy.sh

--- doc/deploy.sh
+++ doc/deploy.sh
@@ -1,2 +1,3 @@
-cd /var/www/html/toplog
-go build; ./toplog
+#!/usr/bin/env bash
+cd /var/www/html/toplog; git pull; go build -o ./toplog; systemctl restart toplog
+systemctl stop toplog; cd /var/www/html/toplog; git pull; go build -o ./toplog; ./toplog
